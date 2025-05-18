5a87cbd3885f191db14c283daaa78a6a0ab722e7
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2025-05-17 20:25:02 -0500

    feat: :construction: Colors and TCP/IP explaination
    All the colors where just changed to the project palette and TCP/IP interactive explaination added.

--- js/scripts/topic4.js
+++ js/scripts/topic4.js
@@ -2,3 +2,18 @@ function toggleSection(id) {
   const section = document.getElementById(id);
   section.classList.toggle('hidden');
 }
+
+function cambiarFlujo() {
+  const stack = document.querySelector(".tcpip-stack");
+  const button = document.getElementById("toggle-flow");
+
+  if (stack.classList.contains("flujo-descendente")) {
+    stack.classList.remove("flujo-descendente");
+    stack.classList.add("flujo-ascendente");
+    button.textContent = "Flujo actual: Recibir ⬆️";
+  } else {
+    stack.classList.remove("flujo-ascendente");
+    stack.classList.add("flujo-descendente");
+    button.textContent = "Flujo actual: Enviar ⬇️";
+  }
+}
