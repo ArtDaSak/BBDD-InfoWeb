10b5e6d58beafea2741b7c718cabda0bf30e36ba
ArtDaSak <182381184+ArtDaSak@users.noreply.github.com>
2025-05-18 10:00:27 -0500

    fix: :bug: Fix size img header in hover

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
     const logo = document.querySelector('.header img');
+    logo.style.maxWidth = '9vw';
 
     if (window.scrollY > 10) {
         header.style.background = 'linear-gradient(to bottom right, rgba(19, 62, 124), rgba(9, 24, 51))';
         logo.src = './assets/img/logos/monoLogo.svg';
         logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--lavander-web))';
         logo.onmouseenter = function () {
-            logo.src = '../assets/img/logos/monoLogo.svg';
             logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--robin-egg-blue-transparent-90))';
         };
         logo.onmouseleave = function () {
-            logo.src = './assets/img/logos/monoLogo.svg';
             logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--lavander-web))';
         };
     } else {
