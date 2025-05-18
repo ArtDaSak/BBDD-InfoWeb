50a75d3d2c262c86ecfb5185ce710184f1ddbcd7
ArtDaSak <182381184+ArtDaSak@users.noreply.github.com>
2025-05-18 09:02:30 -0500

    feat: :sparkles: Improve header function

--- js/util.js
+++ js/util.js
@@ -3,10 +3,20 @@ window.addEventListener('scroll', function () {
     const logo = document.querySelector('.header img');
 
     if (window.scrollY > 10) {
-        header.style.background = 'linear-gradient(to bottom right, rgba(1, 1, 1), rgba(9, 24, 51))';
-        logo.src = './assets/img/logos/monoLogo.svg';
+        header.style.background = 'linear-gradient(to bottom right, rgba(19, 62, 124), rgba(9, 24, 51))';
+        logo.src = '../assets/img/logos/monoLogo.svg';
+        logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--lavander-web))';
+        logo.onmouseenter = function () {
+            logo.src = '../../assets/img/logos/monoLogo.svg';
+            logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--robin-egg-blue-transparent-90))';
+        };
+        logo.onmouseleave = function () {
+            logo.src = '../assets/img/logos/monoLogo.svg';
+            logo.style.filter = 'drop-shadow(0.02vw 0.027vw 0.42vw var(--lavander-web))';
+        };
     } else {
-        header.style.background = 'rgba(9, 24, 51, 0.9)';
-        logo.src = './assets/img/logos/colorLogo.svg';
+        header.style.background = 'rgba(19, 62, 124, 0.75)';
+        logo.src = '../assets/img/logos/colorLogo.svg';
+        logo.style.filter = 'none';
     }
 });
